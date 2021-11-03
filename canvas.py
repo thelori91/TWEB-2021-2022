3a16aa1d3de609df74bb28c20be3f97533bc1acf
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2021-11-03 19:09:42 +0100

    Canvas completato!

--- canvas.py
+++ canvas.py
@@ -1,46 +1,56 @@
 import tkinter as tk
 import math
 
-#get an empty canvas
-def get_canvas(root,width,height):
-    canvas = tk.Canvas(root, bg='grey', width = width, height = height)
-    return canvas
+class Grid:
+    mouse_pressed = False
+    is_drawing = True #true: drawing   false:erasing
 
-#draw on canvas based on its size and the value of a matrix
-def draw_canvas(matrix, canvas):
-    square_height = canvas.winfo_reqheight() / len(matrix)
-    square_width = canvas.winfo_reqheight() / len(matrix[0])
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            coords = [i*square_height, j*square_width, (i*square_height) + square_height, (j * square_width) + square_width]
-            if matrix[i][j] == True:
-                canvas.create_rectangle(coords[0],coords[1],coords[2], coords[3],fill='black',outline = 'green')
-            else:
-               canvas.create_rectangle(coords[0],coords[1],coords[2], coords[3],fill='white', outline = 'green') 
+    #get a Grid object
+    def __init__(self,root,width,height,matrix):
+        self.mouse_pressed = False
+        self.canvas = tk.Canvas(root, bg='grey', width = width, height = height) 
+        self.canvas.bind('<Motion>',lambda event: self.update_matrix(event, matrix))
+        self.canvas.bind('<ButtonPress-1>', lambda event: self.set_flag(True))    
+        self.canvas.bind('<ButtonRelease-1>', lambda event: self.set_flag(False))
+        self.draw_canvas(matrix)
 
-def update_matrix(event):
-    global matrix
-    global canvas
-    print('Click at ' + str(event.x), str(event.y))
-    square_height = canvas.winfo_reqheight() / len(matrix)
-    square_width = canvas.winfo_reqheight() / len(matrix[0])
-    row = math.floor(event.x / square_width)
-    col = math.floor(event.y / square_height)
-    matrix[row][col] = not matrix[row][col]
-    draw_canvas(matrix = matrix, canvas = canvas)
+    def set_flag(self, value):
+        self.mouse_pressed = value
 
+    #should be called every time the matrix has changed
+    def update_matrix(self, event, matrix):
+        if self.mouse_pressed:
+            square_height = self.canvas.winfo_reqheight() / len(matrix)
+            square_width = self.canvas.winfo_reqheight() / len(matrix[0])
+            row = math.floor(event.x / square_width)
+            col = math.floor(event.y / square_height)
+            matrix[row][col] = self.is_drawing
+            self.draw_canvas(matrix = matrix)
+
+    #draw on canvas based on its size and the value of a matrix
+    def draw_canvas(self, matrix):
+        square_height = self.canvas.winfo_reqheight() / len(matrix)
+        square_width = self.canvas.winfo_reqheight() / len(matrix[0])
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                coords = [i*square_height, j*square_width, (i*square_height) + square_height, (j * square_width) + square_width]
+                if matrix[i][j] == True:
+                    self.canvas.create_rectangle(coords[0],coords[1],coords[2], coords[3],fill='black',outline = 'green')
+                else:
+                    self.canvas.create_rectangle(coords[0],coords[1],coords[2], coords[3],fill='white', outline = 'green') 
+   
+    def set_is_drawing(self,value):
+        self.is_drawing = value
 
 top = tk.Tk()
-canvas = get_canvas(top, 600,600)
 
 matrix = [
     [False, False, False],
     [False, False, False],
     [False, False, False]
 ]
-draw_canvas(matrix, canvas)
 
-canvas.bind('<Button-1>', update_matrix)
+grid = Grid(top, 600, 600, matrix)
 
-canvas.pack()
+grid.canvas.pack()
 top.mainloop()
