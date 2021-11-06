ceec6a5aa575a6c483d2003431ef84b9cc494775
Lorenzo Cassinelli <lorenzo.cassinelli@edu.unito.it>
2021-11-06 17:46:52 +0100

    Controllo delle eccezzioni, Stampa a video di cosa succede tramite le azioni e getVal slider

--- FindingPattern.py
+++ FindingPattern.py
@@ -1,70 +1,110 @@
-from tkinter import messagebox
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import *
 from PIL import ImageTk
+import os
+
+# TODO
+# 1 SISTEMARE CODICE
+# 2 SOLUZIONE PER UN TIMER CHE CANCELLI MESSAGGI DELLA printOutLabel NO time.sleep() or Event.wait
+# 3 AGGIUNGERE CANVAS
+# 4 AGGIUNGERE ALGORITMO
+# 5 PENSO LA FINE
 
 root = Tk()
-root.minsize(600, 450)
+root.minsize(600, 500)
 fileRead = False
-root.title("Finding Pattern")
+root.title('Finding Pattern')
 content = ttk.Frame(root, padding=(3, 3, 12, 12))
-frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=200, height=100)
-searchLabel = ttk.Label(content, text="File Name")
-rowLabel = ttk.Label(content, text="Row")
-columnLabel = ttk.Label(content, text="Column")
+frame = ttk.Frame(content, borderwidth=5, relief='ridge', width=200, height=100)
+searchLabel = ttk.Label(content, text='File Name (with extension .txt)')
 fileName = ttk.Entry(content)
+printOutLabel = ttk.Label(content, text='')
+rowLabel = ttk.Label(content, text='Row')
+columnLabel = ttk.Label(content, text='Column')
 
 
-def openFile():  # function that open file with entrybox
+class MyError(Exception):
+    def __init__(self, value):  # codice inutile
+        self.value = value
+
+
+def openFile():  # Function that open a file with the fileName Entrybox
     global fileRead
     if fileRead is False:
-        pathFile = fileName.get()
-        openFileFun = open(pathFile, "r").readlines()
-        rowNumber = len(openFileFun)
-        columnNumber = len(openFileFun[0])
-        print(rowNumber, columnNumber)
-        fileRead = True
+        try:
+            pathFile = fileName.get()
+            if pathFile == '':
+                raise FileNotFoundError
+            file_extension = os.path.splitext(fileName.get())
+            if file_extension[1] == '.txt':
+                openFileFun = open(pathFile, 'r').read().splitlines()
+                rowNumber = len(openFileFun)
+                columnNumber = len(openFileFun[0])
+                fileRead = True
+                print(rowNumber, columnNumber)
+                printOutLabel['text'] = 'File correctly opened'
+            else:
+                raise MyError('File extension not accepted')
+        except FileNotFoundError:
+            printOutLabel['text'] = 'File not found'
+        except MyError:
+            printOutLabel['text'] = 'File extension not accepted'
     else:
-        messagebox.showerror(title="Error file", message="File already opened")
+        printOutLabel['text'] = 'File already opened'
 
 
-def openDialog():  # function that open file with dialog
+def openDialog():  # Function that open a file with dialog
     global fileRead
     if fileRead is False:
         root.filename = filedialog.askopenfilename(initialdir="/", title="Select file",
                                                    filetypes=(("txt files", "*.txt"), ("all files", "*.*")))
-        fileRead = True
-        print(root.filename)
+        if root.filename == '':
+            printOutLabel['text'] = 'No file selected'
+        else:
+            printOutLabel['text'] = 'File correctly opened'
+            fileRead = True
     else:
-        messagebox.showerror(title="Error file", message="File already opened")
+        printOutLabel['text'] = 'File already opened'
 
 
-def resetFindingPattern():  # function that re start application
+def resetFindingPattern():  # Function that restart application
     global fileRead
     fileRead = False
+    printOutLabel['text'] = ''
+
+
+def slider_changed(event):  # DA USARE PER VALORE DELLO SLIDER 1
+    print(int(slider.get()))
+
+
+def slider_changed2(event):  # DA USARE PER VALORE DELLO SLIDER 2
+    print(int(slider2.get()))
 
 
 # Button
-openFileButton = ttk.Button(content, text="Open File", command=openFile)
-photo = ImageTk.PhotoImage(file="fileIMG.png")
+openFileButton = ttk.Button(content, text='Open File', command=openFile)
+photo = ImageTk.PhotoImage(file='fileIMG.png')
 dialogButton = Button(content, image=photo, border=0, command=openDialog)
-resetButton = ttk.Button(content, text="Reset", command=resetFindingPattern)
+resetButton = ttk.Button(content, text='Reset', command=resetFindingPattern)
 file = ttk.Button(content)
 
 # Sliders
-slider = ttk.Scale(content, from_=0, to=100, orient='horizontal')
-slider2 = ttk.Scale(content, from_=0, to=100, orient='horizontal')
+current_value = DoubleVar()
+current_value2 = DoubleVar()
+slider = ttk.Scale(content, from_=1, to=100, orient='horizontal', variable=current_value, command=slider_changed)
+slider2 = ttk.Scale(content, from_=1, to=100, orient='horizontal', variable=current_value2, command=slider_changed)
 
 content.grid(column=0, row=0, sticky=(N, S, E, W))
 frame.grid(column=0, row=0, columnspan=3, rowspan=5, sticky=(N, S, E, W))
 searchLabel.grid(column=3, row=0, columnspan=2, sticky=(N, W), padx=5)
 fileName.grid(column=3, row=1, columnspan=2, sticky=(N, E, W), pady=5, padx=5)
 dialogButton.grid(column=5, row=1, sticky=(N, W), padx=5, pady=8)
-rowLabel.grid(column=3, row=2, sticky=(N, W), padx=5)
-slider.grid(column=3, row=3, sticky=(N, E, W), padx=5)
-columnLabel.grid(column=4, row=2, sticky=(N, W), padx=5)
-slider2.grid(column=4, row=3, sticky=(N, E, W), padx=5)
+printOutLabel.grid(column=3, row=2, columnspan=2, sticky=(N, W), padx=5, pady=70)
+rowLabel.grid(column=3, row=3, sticky=(N, W), padx=5)
+slider.grid(column=3, row=4, sticky=(N, E, W), padx=5)
+columnLabel.grid(column=4, row=3, sticky=(N, W), padx=5)
+slider2.grid(column=4, row=4, sticky=(N, E, W), padx=5)
 openFileButton.grid(column=3, row=4, pady=100)
 resetButton.grid(column=4, row=4, pady=100)
 
