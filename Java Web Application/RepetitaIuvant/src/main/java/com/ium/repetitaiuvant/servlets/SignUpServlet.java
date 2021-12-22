8dfc3e97298157fb3e0a041a115c28b08cc17b1b
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2021-12-22 14:37:12 +0100

    Cleaned a couple missing links to images and stylesheets

---
+++ Java Web Application/RepetitaIuvant/src/main/java/com/ium/repetitaiuvant/servlets/SignUpServlet.java
@@ -0,0 +1,44 @@
+package com.ium.repetitaiuvant.servlets;
+
+import com.ium.repetitaiuvant.DAO.DAO;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "signUpServlet", value = "/signUp-servlet")
+public class SignUpServlet extends HttpServlet {
+
+    public void init(ServletConfig conf) throws ServletException {
+        super.init(conf);
+        ServletContext ctx = conf.getServletContext();
+        String url = ctx.getInitParameter("DB-URL");
+        String user = ctx.getInitParameter("user");
+        String pwd = ctx.getInitParameter("psw");
+        DAO.initDAO(url, user, pwd);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Ciao");
+        processRequest(request, response);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    public void destroy() {
+    }
+}
